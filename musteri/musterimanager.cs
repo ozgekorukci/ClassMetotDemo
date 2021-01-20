ae52ad46458a603b47bacf2a28d08942751d93cd
ozgekorukci <20181101053@ogr.msgsu.edu.tr>
2021-01-20 22:17:55 +0300

    Odev

---
+++ musteri/musterimanager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Eklenen müşteri: " + musteri.MusteriAdi + " " + musteri.MusteriSoyadi);
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Silinen müşteri: " + musteri.MusteriAdi + " " + musteri.MusteriSoyadi);
+        }
+
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi);
+        }
+    }
+}
